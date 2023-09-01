1de8477ddcff8f7b458170e8693485a44c43547e
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2023-09-01 19:58:11 -0400

    adding more pieces in board
    TODO: mejorar la forma en la que los cells descriptors updatean el cell_range

--- Core/Players/player.gd
+++ Core/Players/player.gd
@@ -3,20 +3,34 @@ extends Node2D
 
 @export var king_unit_scene: PackedScene
 var enemy_player : Player
+
+var pieces_scenes: Dictionary = {
+	"king": "res://Core/Units/king.tscn",
+	"bishop": "res://Core/Units/bishop.tscn",
+}
+var position_unit_array: Array[Array] = [
+	["king", Vector2(0, 0)],
+	["bishop", Vector2(-30, 0)],
+	["bishop", Vector2(30, 0)],
+]
 # Public
 
 # Sets up the multiplayer data for the player node
 func multiplayer_setup(peer_player: MultiplayerManager.PeerPlayer):
 	name = "Player" + str(peer_player.id)
+	for i in range(position_unit_array.size()):
+		var offset = position_unit_array[i][1]
+		var piece_name = position_unit_array[i][0]
+		var king_unit = load(pieces_scenes[piece_name]).instantiate() # cant preload as cell_descriptors get added before GameManager
 		
-	var king_unit = king_unit_scene.instantiate()
-	add_child(king_unit)
-	
-	king_unit.name = "King" + str(peer_player.id)
-	
-	if multiplayer.get_unique_id() == peer_player.id:
-		king_unit.position = GameManager.map.get_initial_king_position()
-	else:
-		king_unit.position = GameManager.board.get_mirror_position(GameManager.map.get_initial_king_position())
-	
+		add_child(king_unit)
+		
+		king_unit.name = str(i) + str(peer_player.id)
+		
+		if multiplayer.get_unique_id() == peer_player.id:
+			king_unit.position = GameManager.map.get_initial_king_position() + offset
+		else:
+			king_unit.position = GameManager.board.get_mirror_position(GameManager.map.get_initial_king_position() + offset)
+		
 	set_multiplayer_authority(peer_player.id)
+	GameManager.set_board(GameManager.board) # unit's cell_descriptors aren't updating as board was set before
