277fb463393d48d58b4c644f9b0aa533722c2a55
Juanxpeke <juanintendo.f11@gmail.com>
2023-10-07 15:13:22 -0300

    feat: added turn indicator

--- Core/UI/HUD/PlayerInterface/player_interface.gd
+++ Core/UI/HUD/PlayerInterface/player_interface.gd
@@ -1,8 +1,11 @@
 class_name PlayerInterface
 extends Control
 
+@export var turn_indicator_color: Color
+
 var player: Player
 
+@onready var peer_name_label: Label = %PeerNameLabel
 @onready var life_bar: TextureProgressBar = %LifeBar
 @onready var life_label: Label = %LifeLabel
 
@@ -15,17 +18,34 @@ func _ready() -> void:
 # Called when the related player health changes
 func _on_player_health_changed() -> void:
 	_update_life_interface()
+	
+# Called when a match turn ends
+func _on_turn_ended() -> void:
+	_update_turn_indicator()
 
 # Updates the life interface
 func _update_life_interface() -> void:
 	life_bar.value = player.current_health
 	life_label.text = str(player.current_health) + " / "  + str(player.role.initial_health)
 
+# Updates the turn indicator
+func _update_turn_indicator() -> void:
+	if GameManager.map.get_current_turn_player() == player:
+		peer_name_label.add_theme_color_override("font_color", turn_indicator_color)
+	else:
+		peer_name_label.remove_theme_color_override("font_color")
+
 # Public
 
 # Sets the related player
 func set_player(player: Player) -> void:
 	self.player = player
+	
+	peer_name_label.text = player.peer_player.name
+	
+	_update_turn_indicator()
+	
+	GameManager.map.turn_ended.connect(_on_turn_ended)
 	
 	life_bar.max_value = player.role.initial_health
 	_update_life_interface()
