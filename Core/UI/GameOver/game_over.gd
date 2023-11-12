91894b04ada9be5c3af56bc5c636d1e31819e9cd
Juanxpeke <juanintendo.f11@gmail.com>
2023-11-12 20:49:36 -0300

    feat: added better game over UI

--- Core/UI/GameOver/game_over.gd
+++ Core/UI/GameOver/game_over.gd
@@ -2,18 +2,21 @@ extends CanvasLayer
 
 @export var main_menu_scene: PackedScene
 
+@onready var win_icon := %WinIcon
 @onready var win_label := %WinLabel
-@onready var button := %Button
+@onready var go_main_menu_button := %GoMainMenuButton
 
 # Private
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var win_role = RolesManager.get_role(GameManager.last_winner.role_id)
+	win_icon.texture = win_role.icon 
 	win_label.text = "%s WINS!" % GameManager.last_winner.name.to_upper()
 	
-	button.pressed.connect(_button_pressed)
+	go_main_menu_button.pressed.connect(_go_main_menu_button_pressed)
 
 # Called when the menu button is pressed
-func _button_pressed():
+func _go_main_menu_button_pressed():
 	multiplayer.multiplayer_peer.close()
 	get_tree().change_scene_to_packed(main_menu_scene)
