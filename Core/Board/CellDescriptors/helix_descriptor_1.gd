3af9300ad845c59d44ecac8ab0f757d68edfcd70
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2023-11-13 23:54:34 -0300

    fix ninja cell_descriptor(¿)

--- Core/Board/CellDescriptors/helix_descriptor_1.gd
+++ Core/Board/CellDescriptors/helix_descriptor_1.gd
@@ -6,18 +6,8 @@ func get_directions() -> Array[Array]:
 	return [
 		# We want the piece to be blockable
 		# These directions dont allow it
-		[Vector2i(0, 1)],
-		[Vector2i(-1, 0)],
-		[Vector2i(0, -1)],
-		[Vector2i(1, 0)],
-		
-		[Vector2i(0, 2)],
-		[Vector2i(-2, 0)],
-		[Vector2i(0, -2)],
-		[Vector2i(2, 0)],
-		
-		[Vector2i(-2, 1)],
-		[Vector2i(1, 2)],
-		[Vector2i(-1, -2)],
-		[Vector2i(2, -1)],
+		[Vector2i(0, 1), Vector2i(0, 1), Vector2i(1, 0)],
+		[Vector2i(-1, 0), Vector2i(-1, 0), Vector2i(0, 1)],
+		[Vector2i(0, -1), Vector2i(0, -1), Vector2i(-1, 0)],
+		[Vector2i(1, 0), Vector2i(1, 0), Vector2i(0, -1)],
 	]
--- Core/Board/CellDescriptors/helix_descriptor_3.gd
+++ Core/Board/CellDescriptors/helix_descriptor_3.gd
@@ -6,28 +6,8 @@ func get_directions() -> Array[Array]:
 	return [
 		# We want the piece to be blockable
 		# These directions dont allow it
-		[Vector2i(0, 1)],
-		[Vector2i(-1, 0)],
-		[Vector2i(0, -1)],
-		[Vector2i(1, 0)],
-		
-		[Vector2i(0, 2)],
-		[Vector2i(-2, 0)],
-		[Vector2i(0, -2)],
-		[Vector2i(2, 0)],
-		
-		[Vector2i(-2, 1)],
-		[Vector2i(1, 2)],
-		[Vector2i(-1, -2)],
-		[Vector2i(2, -1)],
-		
-		[Vector2i(-2, 2)],
-		[Vector2i(2, 2)],
-		[Vector2i(-2, -2)],
-		[Vector2i(2, -2)],
-		
-		[Vector2i(-2, 3)],
-		[Vector2i(3, 2)],
-		[Vector2i(-3, -2)],
-		[Vector2i(2, -3)]
+		[Vector2i(0, 1), Vector2i(0, 1), Vector2i(1, 0), Vector2i(1, 0), Vector2i(1, 0)],
+		[Vector2i(-1, 0), Vector2i(-1, 0), Vector2i(0, 1), Vector2i(0, 1), Vector2i(0, 1)],
+		[Vector2i(0, -1), Vector2i(0, -1), Vector2i(-1, 0), Vector2i(-1, 0), Vector2i(-1, 0)],
+		[Vector2i(1, 0), Vector2i(1, 0), Vector2i(0, -1), Vector2i(0, -1), Vector2i(0, -1)],
 	]
