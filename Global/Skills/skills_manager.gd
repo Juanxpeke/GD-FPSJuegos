8074d451b44bf4864ad9925a9f6df8e038726156
shejogalvez <71142873+shejogalvez@users.noreply.github.com>
2023-11-23 19:19:17 -0300

    fix path

--- Global/Skills/skills_manager.gd
+++ Global/Skills/skills_manager.gd
@@ -5,13 +5,13 @@ var skills = [
 	# Passives
 	"res://Global/Skills/DimensionalJump/dimensional_jump.tres",
 	"res://Global/Skills/JumperAscension/jumper_ascension.tres",
-	"res://Global/Skills/Reinforcements/reinforcements.tres",
-	"res://Global/Skills/DimensionalJump/dimensional_jump_v.tres",
 	# Consumables
 	"res://Global/Skills/Consumables/PirateFortune/pirate_fortune.tres",
 	"res://Global/Skills/Consumables/JumperLevelUp/jumper_level_up.tres",
 	# Actives
 	"res://Global/Skills/Actives/Ghost/ghost.tres",
+	"res://Global/Skills/Actives/Reinforcements/reinforcements.tres",
+	"res://Global/Skills/Actives/DimensionalJump/dimensional_jump_v.tres",
 ]
 
 # Public
